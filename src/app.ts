fe2cae3148648f4930776f1f956dc02d3470bec6
mdfahimpatowry <fahimjoy768@gmail.com>
2026-02-09 02:07:28 +0600

    swagger setup

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,15 @@
 import express from "express";
+import swaggerUi from "swagger-ui-express";
+import swaggerSpec from "./config/swagger";
+
 import healthRoute from "./routes/health.route";
 
 const app = express();
 
 app.use(express.json());
+
+// Swagger setup
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 // routes
 app.use("/", healthRoute);
--- src/routes/health.route.ts
+++ src/routes/health.route.ts
@@ -2,6 +2,15 @@ import { Router } from "express";
 
 const router = Router();
 
+/**
+ * @swagger
+ * /api/health:
+ *   get:
+ *     summary: Returns OK status
+ *     responses:
+ *       200:
+ *         description: OK message
+ */
 router.get("/health", (_req, res) => {
   res.status(200).send("ok");
 });
