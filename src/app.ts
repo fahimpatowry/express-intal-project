287d9c8d6ee5e1b1b731a63c2e90f6ef226a5837
mdfahimpatowry <fahimjoy768@gmail.com>
2026-02-09 00:48:00 +0600

    health route and mongo

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,11 @@
 import express from "express";
+import healthRoute from "./routes/health.route";
 
 const app = express();
 
 app.use(express.json());
+
+// routes
+app.use("/", healthRoute);
 
 export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,20 @@
 import app from "./app";
 import "dotenv/config";
+import connectDB from "./config/db";
 
 const PORT = process.env.PORT || 4000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on ${PORT}`);
-});
+const startServer = async () => {
+  try {
+    await connectDB();
+
+    app.listen(PORT, () => {
+      console.log(`🚀 Server running on ${PORT}`);
+    });
+  } catch (error) {
+    console.error("❌ Failed to start server", error);
+    process.exit(1);
+  }
+};
+
+startServer();
